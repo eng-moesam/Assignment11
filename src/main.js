3c632b56e9baf65e331c4751a9e6f9805bc52757
eng-moesam <moesam224466@gmail.com>
2026-04-10 17:38:39 +0200

    third bootstrab fix

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import bootstrap from "./app.bootstrap.js";
 // import app from "./app.bootstrap.js"
 // export default app
-export default bootstrap
+// export default bootstrap
+export default bootstrap()
